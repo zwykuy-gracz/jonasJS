30a2ae086bb924b145d045b337318cb91ef1f040
Paul Specjał <Paul.Specjal@intrum.com>
2021-06-23 15:30:10 +0200

    before challenge #2

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -93,44 +93,27 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
-///////////////////////////////////////
-const checkDogs = function (dogsJulia, dogsKate) {
-  const dogsJuliaCorrected = dogsJulia.slice();
-  dogsJuliaCorrected.splice(0, 1);
-  dogsJuliaCorrected.splice(-2);
-  const dogs = dogsJuliaCorrected.concat(dogsKate);
-  console.log(dogs);
-  dogs.forEach(function (dog, i) {
-    if (dog >= 3) {
-      console.log(`Dog no ${i + 1} is an adoult, and is ${dog} years old`);
-    } else {
-      console.log(`Dog no ${i + 1} is still a puppy`);
-    }
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
   });
 };
-// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+createUsernames(accounts);
+// console.log(accounts);
 
-const eurToUsd = 1.1;
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-// const movementsUSD = movements.map(function (mov) {
-//   return mov * eurToUsd;
-// });
-
-const movementsUSD = movements.map(mov => mov * eurToUsd);
-
-// console.log(movements);
-// console.log(movementsUSD);
-
-const movementsUSDfor = [];
-for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
-// console.log(movementsUSDfor);
-
-const movementsDescriptions = movements.map(
-  (mov, i, arr) =>
-    `movemnt ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'}  ${Math.abs(
-      mov
-    )}`
-);
-
-console.log(movementsDescriptions);
+// Max value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
