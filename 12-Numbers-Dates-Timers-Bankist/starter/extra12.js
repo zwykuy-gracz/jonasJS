45895eac5da28c17eabf05e581836ae358bcfb45
Paul Specjał <Paul.Specjal@intrum.com>
2021-07-08 14:37:48 +0200

    starting 12

---
+++ 12-Numbers-Dates-Timers-Bankist/starter/extra12.js
@@ -0,0 +1,6 @@
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
