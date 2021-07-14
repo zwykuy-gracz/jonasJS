4ccf1cb76176f26c5eaf39b2479139dda0f6d0a9
Paul Specjał <Paul.Specjal@intrum.com>
2021-07-14 16:57:22 +0200

    14

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const paul = new Person('Paul', 1988);
+
+// Prototypes
+Person.prototype.calcAge = this.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+paul.calcAge();
+console.log(paul.__proto__);
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(paul.species);
