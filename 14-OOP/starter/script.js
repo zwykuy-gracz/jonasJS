52bf452d22c1e285d303c28afea3e203c2b4ce30
Paul Specjał <Paul.Specjal@intrum.com>
2021-07-15 12:29:07 +0200

    car

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -12,8 +12,26 @@ Person.prototype.calcAge = this.calcAge = function () {
   console.log(2037 - this.birthYear);
 };
 
-paul.calcAge();
-console.log(paul.__proto__);
+// paul.calcAge();
+// console.log(paul.__proto__);
 
 Person.prototype.species = 'Homo Sapiens';
-console.log(paul.species);
+// console.log(paul.species);
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const car1 = new Car('BMW', 120);
+
+Car.prototype.accelerate = this.accelerate = function () {
+  console.log(`'${this.make}' going at ${this.speed + 10}`);
+};
+
+Car.prototype.brake = this.brake = function () {
+  console.log(`'${this.make}' going at ${this.speed - 5}`);
+};
+
+car1.accelerate();
+car1.brake();
