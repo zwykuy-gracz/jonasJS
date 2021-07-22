48a6ca4dc2417e084aa377951e1d9529353dd437
Paul Specjał <Paul.Specjal@intrum.com>
2021-07-22 16:14:54 +0200

    uff

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,11 @@
+// Importing module
+// import { addToCart, tq, totalPrice as price } from './shoppingCart.js';
+
+// import * as shoppingCart from './shoppingCart.js';
+console.log('Importing module');
+
+// shoppingCart.addToCart('bread', 5);
+// console.log(shoppingCart.totalPrice, shoppingCart.tq);
+
+import add from './shoppingCart.js';
+add('pizza', 2);
---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,20 @@
+// Exporting module
+console.log('Exporting module');
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 234;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
