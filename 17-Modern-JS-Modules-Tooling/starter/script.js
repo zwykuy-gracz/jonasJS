289232db420a6ed14e7357612aec5470e9e5cd75
Paul Specjał <Paul.Specjal@intrum.com>
2021-07-22 16:23:54 +0200

    sleeping

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -7,5 +7,31 @@ console.log('Importing module');
 // shoppingCart.addToCart('bread', 5);
 // console.log(shoppingCart.totalPrice, shoppingCart.tq);
 
-import add from './shoppingCart.js';
-add('pizza', 2);
+// import add from './shoppingCart.js';
+// add('pizza', 2);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 234;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+ShoppingCart2.addToCart('apples', 4);
+ShoppingCart2.addToCart('pizzas', 2);
+console.log(ShoppingCart2);
